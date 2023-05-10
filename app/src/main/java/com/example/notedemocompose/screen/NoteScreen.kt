5c23b0663e096c4fc606d108a0109748d02adec6
admin <mingaleev.d92@mail.ru>
2023-05-10 18:04:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notedemocompose/screen/NoteScreen.kt
@@ -0,0 +1,20 @@
+package com.example.notedemocompose.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * @author : Mingaleev D
+ * @data : 10.05.2023
+ */
+
+@Composable
+fun NoteScreen() {
+
+}
+
+@Preview
+@Composable
+fun NoteScreenPreview() {
+   NoteScreen()
+}
